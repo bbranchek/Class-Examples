e78fd5ec7e42ec140bc34caf0f42d0f9600e6d71
bbranchek <137911034+bbranchek@users.noreply.github.com>
2025-03-30 10:58:07 -0400

    Add files via upload

---
+++ IF NAME MAIN EXAMPLE NEWEST/namemain.py
@@ -0,0 +1,6 @@
+def addtwo(x,y):
+	result = x + y
+	return result
+if __name__ == "__main__":
+	print('The two numbers are added together')
+
---
+++ IF NAME MAIN EXAMPLE NEWEST/namemainnoifmain.py
@@ -0,0 +1,5 @@
+def addtwo(x,y):
+	result = x + y
+	return result
+print('The two numbers are added together')
+
